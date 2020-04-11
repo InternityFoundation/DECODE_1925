e9707a17de37be4d00c0ef38858fc747c1bd1eb9
Dhanesh chaudhary <44636841+youngbuck09@users.noreply.github.com>
2020-04-12 01:50:35 +0530

    Update readJson.py

--- readJson.py
+++ readJson.py
@@ -1,3 +1,16 @@
 import json,ast
 
 tweets=[]
+
+with open('twts4.txt','r') as f:
+    while True:
+        t=f.readline()
+        if (not t):
+            break
+        tweets.append(ast.literal_eval(t))
+
+print(len(tweets))
+
+ids=[]
+
+#print(len(tweets))
