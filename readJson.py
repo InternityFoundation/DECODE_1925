ee0031ffd7387f13680ed2eac55d90748e0e307d
Dhanesh chaudhary <44636841+youngbuck09@users.noreply.github.com>
2020-04-12 02:33:56 +0530

    Update readJson.py

--- readJson.py
+++ readJson.py
@@ -14,3 +14,22 @@ print(len(tweets))
 ids=[]
 
 #print(len(tweets))
+
+class User:
+    def __init__(self,id,name,fllwr):
+        self.id=id
+        self.name=name
+        self.fllwr=fllwr
+users=[]
+for tweet in tweets:
+    u=User(tweet['user']['id'],tweet['user']['screen_name'],tweet['user']['followers_count'])
+    if (u.id not in ids and tweet['user']['verified']==1):
+        users.append(u)
+        ids.append(tweet['user']['id'])
+
+    if ('retweeted_status' in tweet.keys()):
+        u=User(tweet['retweeted_status']['user']['id'],tweet['retweeted_status']['user']['screen_name'],tweet['retweeted_status']['user']['followers_count'])
+        if (u.id not in ids and tweet['retweeted_status']['user']['verified']==1) :
+            users.append(u)
+            ids.append(tweet['retweeted_status']['user']['id'])
+
