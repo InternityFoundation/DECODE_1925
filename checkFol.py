19342d2e97b81667cef6c648451bb570049ef85d
harshitabajaj <63507655+harshitabajaj@users.noreply.github.com>
2020-04-12 04:19:44 +0530

    Update checkFl.py

--- checkFol.py
+++ checkFol.py
@@ -9,3 +9,15 @@ access_token_secret='TCq5nvL8pEzYZZkum1alJInylMbcvzZqiysTFqadVaW0M'
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+import pandas as pd
+
+df=pd.read_csv('users1.csv',header=None)
+
+df.columns=['id','handle','followers']
+#print(df.head())
+usrs=list(df['id'])
+#print(usrs)
+folls=[]
+#print(api.show_friendship(source_id=207809313,target_id=1015365007))
+
